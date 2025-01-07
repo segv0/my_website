77bc1fa1f2b5222ddbe3ec04221ae81900fb45dc
Arnav K <arnavkaushal09@gmail.com>
2025-01-07 18:43:14 +0000

    base

---
+++ src/consts.ts
@@ -0,0 +1,16 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Token';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAV_LINKS: Array<{title: string, href?:string}> = [{
+    title: 'Blog'
+},
+{
+    title: 'Github',
+    href:"//github.com/ArnavK-09"
+},{
+    title: 'Source',
+    href:"//github.com/ArnavK-09"
+},
+]
