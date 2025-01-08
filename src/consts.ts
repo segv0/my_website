deabff47de36de8ccbdf6e3d6dedff3e91f4f9f0
Arnav K <arnavkaushal09@gmail.com>
2025-01-08 18:00:15 +0000

    prose left

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,23 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Token';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const NAV_LINKS: Array<{title: string, href?:string}> = [{
-    title: 'Blog'
-},
-{
-    title: 'Github',
-    href:"//github.com/ArnavK-09"
-},{
-    title: 'Source',
-    href:"//github.com/ArnavK-09"
-},
-]
+export const SITE_TITLE = "Token";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const KNOWN_TECH =
+  `Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX,Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX`.split(
+    ",",
+  );
+export const ABOUT_ME =
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus.`.trim();
+export const GITHUB_USERNAME = "ArnavK-09";
+export const QUOTE = "Programmer, Gamer, Editor and Student";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Blog",
+  },
+  {
+    title: "Github",
+    href: "//github.com/" + GITHUB_USERNAME,
+  },
+  {
+    title: "Source",
+    href: "//github.com/ArnavK-09/token-template",
+  },
+];
