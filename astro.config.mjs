ef5c2dc1a808f498359502c19ce91103456298fb
Arnav K <arnavkaushal09@gmail.com>
2025-01-08 18:32:36 +0000

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: "https://token-template.deno.dev",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
