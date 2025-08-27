bf5139d90d7c372949d30db78b1ab18eac2ec09c
Mehmet Eren Alpan <alpanmemo@gmail.com>
2025-08-27 20:42:33 +0300

    html

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE = "segv0";
 export const SITE_DESCRIPTION = `CTF Writeups and Cybersecurity Blogs`.trim();
 
 export const KNOWN_TECH =
-  `PHP,CSS,HTML,Burp Suite,Bash,Python,Linux,Java,HTML,Ghidra,SQL,C,C#`.split(",");
+  `PHP,CSS,HTML,Burp Suite,Bash,Python,Linux,Java,Ghidra,SQL,C,C#`.split(",");
 export const ABOUT_ME =
   `I’m segv0, a cybersecurity enthusiast and CTF player. My journey began at a young age when my dad introduced me to coding, sparking a curiosity that grew into robotics projects like FRC and later into the world of cybersecurity. Over time, I deepened my skills, earned my CEH certification, and immersed myself in CTFs, exploit development, and reverse engineering. This blog is where I share my writeups, insights, and lessons learned along the way. `.trim();
 export const GITHUB_USERNAME = "segv0";
